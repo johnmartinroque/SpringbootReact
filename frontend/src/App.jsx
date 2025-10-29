018b928a7c6c9db0273e3ea1a975a281e50c1239
johnmartinroque <johnmartinroque@gmail.com>
2025-10-29 15:33:12 +0800

    tailwind fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import { BrowswerRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./screens/Register";
+import Login from "./screens/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
