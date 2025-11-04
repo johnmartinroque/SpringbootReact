bf954edc474634070c5fc610adcea595a4d5238f
johnmartinroque <johnmartinroque@gmail.com>
2025-11-04 11:05:11 +0800

    remove

--- frontend/src/components/TodoComponent.jsx
+++ frontend/src/components/TodoComponent.jsx
@@ -9,6 +9,11 @@ function TodoComponent() {
     setNewTodo("");
     console.log(todos);
   };
+
+  const handleRemove = (index) => {
+    const filtered = todos.filter((_, i) => i !== index);
+    setTodos(filtered);
+  };
   return (
     <div className="max-w-2xl">
       TodoComponent
@@ -35,7 +40,16 @@ function TodoComponent() {
         Default
       </button>
       {todos.map((todo, index) => (
-        <h1 key={index}>{todo}</h1>
+        <div>
+          <h1 key={index}>{todo}</h1>
+          <button
+            onClick={() => handleRemove(index)}
+            type="button"
+            class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+          >
+            Remove
+          </button>
+        </div>
       ))}
     </div>
   );
